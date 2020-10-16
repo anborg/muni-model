b2435224e39c7e3c2cf23e48e0de43459f928ee9
anborg <dev@gmail.com>
2020-10-16 15:51:16 -0400

    proto_null.json - rountrip ser/de test

--- src/main/java/muni/util/ProtoUtil.java
+++ src/main/java/muni/util/ProtoUtil.java
@@ -1,7 +1,12 @@
 package muni.util;
+import com.google.common.base.Strings;
 import com.google.protobuf.Message;
 import com.google.protobuf.MessageOrBuilder;
 import com.google.protobuf.util.JsonFormat;
+
+import java.util.Objects;
+import java.util.Optional;
+
 /**
  * Author @espresso stackoverflow.
  * Sample use:
@@ -10,22 +15,34 @@ import com.google.protobuf.util.JsonFormat;
  * final String resJson = ProtoUtil.toJson(res);
  **/
 public class ProtoUtil {
-    public static <T extends Message> String toJson(T obj) {
+
+    public static <T extends Message> Optional<String> toJson(T obj) {
+        if(isNullable(obj)) return Optional.empty();
         try {
-            return JsonFormat.printer().print(obj);
+            return Optional.of(JsonFormat.printer().print(obj));
         } catch (Exception e) {
             throw new RuntimeException("Error converting Proto to json", e);
         }
-    }
+    }//toJson
 
-    public static <T extends MessageOrBuilder> T toProto(String protoJsonStr, T message) {
+    public static <T extends MessageOrBuilder> Optional<T> toProto(String protoJsonStr, T message) {
+        //TODO perhaps for null, I have to return defaultInstance() ?
+        if(isNullable(protoJsonStr)) return Optional.empty();
         try {
             Message.Builder builder = message.getDefaultInstanceForType().toBuilder();
             JsonFormat.parser().ignoringUnknownFields().merge(protoJsonStr, builder);
             T out = (T) builder.build();
-            return out;
+            return Optional.of(out);
         } catch (Exception e) {
             throw new RuntimeException("Error converting json to Proto", e);
         }
+    }//toProto
+    static final String EMPTY_JSON = "{}";
+
+    public static boolean isNullable(Message message){
+        return (Objects.isNull(message) || Strings.isNullOrEmpty(message.toString()));
+    }
+    public static boolean isNullable(String jsonStr){
+        return (Strings.isNullOrEmpty(jsonStr) || jsonStr.contentEquals(EMPTY_JSON));
     }
 }
--- src/test/java/muni/model/TestProtoUtilSerde.java
+++ src/test/java/muni/model/TestProtoUtilSerde.java
@@ -5,6 +5,8 @@ import muni.util.MockUtil;
 import muni.util.ProtoUtil;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,9 +17,24 @@ public class TestProtoUtilSerde {
         //with
         String jsonStr = ClasspathUtil.readFileInClasspath("person.json");
         //when
-        Model.Person p = ProtoUtil.toProto(jsonStr, Model.Person.getDefaultInstance());
-        String jsonStrOut = ProtoUtil.toJson(p);
+        Optional<Model.Person> p = ProtoUtil.toProto(jsonStr, Model.Person.getDefaultInstance());
+        Optional<String> jsonStrOut = ProtoUtil.toJson(p.get());
+        assertThat(jsonStrOut.isPresent()).isTrue();
         //then - verify
-        assertEquals(mapper.readTree(jsonStr), mapper.readTree(jsonStrOut));
+        assertEquals(mapper.readTree(jsonStr), mapper.readTree(jsonStrOut.get()));
+    }
+    @Test
+    public void test_emptyJson_serde_as_emptyJson()throws Exception{
+        //with
+        String jsonStr = ClasspathUtil.readFileInClasspath("person_null.json");
+        System.out.println("jsonStr=" + jsonStr);
+        //when
+        Optional<Model.Person> p = ProtoUtil.toProto(jsonStr, Model.Person.getDefaultInstance());
+        assertThat(p.isPresent()).isFalse();
+        Optional<String> jsonStrOut = ProtoUtil.toJson(null);
+        System.out.println("empty p=" + p);
+        //then - verify
+        assertThat(jsonStrOut.isPresent()).isFalse();
+        //assertEquals(mapper.readTree(jsonStr), mapper.readTree(jsonStrOut));
     }
 }
