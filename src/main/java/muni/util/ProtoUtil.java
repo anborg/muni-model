6e2fd8865a235da6b04dda51e5b69897eb1f06d6
anborg <dev@gmail.com>
2020-10-15 20:01:30 -0400

    data quality test added

--- src/main/java/muni/util/ProtoUtil.java
+++ src/main/java/muni/util/ProtoUtil.java
@@ -1,24 +1,30 @@
 package muni.util;
-
 import com.google.protobuf.Message;
 import com.google.protobuf.MessageOrBuilder;
 import com.google.protobuf.util.JsonFormat;
-
+/**
+ * Author @espresso stackoverflow.
+ * Sample use:
+ * Model.Person reqObj = ProtoUtil.toProto(reqJson, Model.Person.getDefaultInstance());
+ * Model.Person res = personSvc.update(reqObj);
+ * final String resJson = ProtoUtil.toJson(res);
+ **/
 public class ProtoUtil {
-    public static <T extends Message> String toJson(T obj){
-        try{
+    public static <T extends Message> String toJson(T obj) {
+        try {
             return JsonFormat.printer().print(obj);
-        }catch(Exception e){
+        } catch (Exception e) {
             throw new RuntimeException("Error converting Proto to json", e);
         }
     }
-    public static <T extends MessageOrBuilder> T toProto(String protoJsonStr, T message){
-        try{
+
+    public static <T extends MessageOrBuilder> T toProto(String protoJsonStr, T message) {
+        try {
             Message.Builder builder = message.getDefaultInstanceForType().toBuilder();
-            JsonFormat.parser().ignoringUnknownFields().merge(protoJsonStr,builder);
+            JsonFormat.parser().ignoringUnknownFields().merge(protoJsonStr, builder);
             T out = (T) builder.build();
             return out;
-        }catch(Exception e){
+        } catch (Exception e) {
             throw new RuntimeException("Error converting json to Proto", e);
         }
     }
--- src/test/java/muni/model/TestProtoUtilSerde.java
+++ src/test/java/muni/model/TestProtoUtilSerde.java
@@ -1,6 +1,7 @@
 package muni.model;
 
 import muni.util.MockUtil;
+import muni.util.ProtoUtil;
 import org.junit.jupiter.api.Test;
 
 public class TestProtoUtilSerde {
@@ -8,14 +9,6 @@ public class TestProtoUtilSerde {
     @Test
     public void test_toJson() throws Exception{
         Model.Person p = MockUtil.buildPerson();
-
-//        Map<Descriptors.FieldDescriptor, Object> fields = p.getAllFields();
-//        fields.forEach( (m,n) -> System.out.println(m.getName() + " " + n.toString()));
-
-//        ProtoUtil.toJson();
-//        System.out.println(p.toByteString());
-//        p.writeDelimitedTo(System.out);
-//        p.writeTo(System.out);
-
+        ProtoUtil.toJson(p);
     }
 }
