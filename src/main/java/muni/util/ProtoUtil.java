4c291add9e2da361638fbec4d5c7ae170b70545a
anborg <dev@gmail.com>
2020-11-28 20:14:34 -0500

    remove schema prefix "integ"

--- src/main/java/muni/util/ProtoUtil.java
+++ src/main/java/muni/util/ProtoUtil.java
@@ -4,8 +4,10 @@ import com.google.protobuf.Message;
 import com.google.protobuf.MessageOrBuilder;
 import com.google.protobuf.util.JsonFormat;
 
+import java.util.Collection;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Author @espresso stackoverflow.
@@ -15,7 +17,33 @@ import java.util.Optional;
  * final String resJson = ProtoUtil.toJson(res);
  **/
 public class ProtoUtil {
+    private static String EMPTY_JSON_ARRAY = "[]";
 
+    /**
+     * Convert a collection of Protobuf  to a JSON array string  ->
+     * @param listOfProto collection of ..
+     * @param <T> a Protobuf Message
+     * @return a json arrau
+     */
+    public static <T extends Message> String toJsonArray(Collection<T> listOfProto) {
+        if(Objects.isNull(listOfProto)) throw new RuntimeException("Don't give null to me!");
+        if(listOfProto.isEmpty()) return EMPTY_JSON_ARRAY;//empty json
+        try {
+            String jsonArray = listOfProto.stream()
+                    .map(x ->toJson(x))
+                    .collect(Collectors.joining(","));
+            return "[" + jsonArray + "]";
+        } catch (Exception e) {
+            throw new RuntimeException("Error converting Collection<Proto> to json", e);
+        }
+    }//toJsonArray
+
+    /**
+     * Convert ONE protobuf object to JSON string
+     * @param obj input, a protobuf Message
+     * @param <T>
+     * @return json String
+     */
     public static <T extends Message> String toJson(T obj) {
         if(isNullable(obj)) return null;
         try {
@@ -25,6 +53,13 @@ public class ProtoUtil {
         }
     }//toJson
 
+    /**
+     * Convert ONE Json string equivalent of protobufMessage to a Protobuf object
+     * @param protoJsonStr
+     * @param message, pass a Message.getDefaultInstance(), e,g Model.Person.getDefaultInstance()
+     * @param <T>
+     * @return a Protobuf Message
+     */
     public static <T extends MessageOrBuilder> Optional<T> toProto(String protoJsonStr, T message) {
         //TODO perhaps for null, I have to return defaultInstance() ?
         if(isNullable(protoJsonStr)) return Optional.empty();
@@ -39,10 +74,22 @@ public class ProtoUtil {
     }//toProto
     static final String EMPTY_JSON = "{}";
 
+    /**
+     * Check if a protobuf Message is semantically-equal to null/notfound ...
+     *
+     * @param message
+     * @return
+     */
     public static boolean isNullable(Message message){
         return (Objects.isNull(message)
                 || Strings.isNullOrEmpty(message.toString()));
     }
+
+    /**
+     * Check if a json string can be semantically equated to null/notfound ...
+     * @param jsonStr
+     * @return
+     */
     public static boolean isNullable(String jsonStr){
         return (Strings.isNullOrEmpty(jsonStr) || jsonStr.contentEquals(EMPTY_JSON));
     }
